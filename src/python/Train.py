b071248a0a753907b3afa9f035d7eb3e718b69e3
brandonang242-sudo <brandonang242@gmail.com>
2026-02-11 18:33:59 +0800

    Update Train.py

--- src/python/Train.py
+++ src/python/Train.py
@@ -3,10 +3,9 @@ from transformers import Trainer, TrainingArguments
 training_args = TrainingArguments(
     output_dir="./results",
     learning_rate=2e-5,
-    per_device_train_batch_size=1,  
-    gradient_accumulation_steps=8,  
-    num_train_epochs=3,             
-    weight_decay=0.01,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    num_train_epochs=3,
     logging_steps=10,
     save_steps=50,
     evaluation_strategy="steps"
@@ -16,8 +15,7 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["test"],
+    eval_dataset=tokenized_datasets["test"]
 )
 
-# Train on CPU
 trainer.train()
